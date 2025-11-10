a05ea166cbf1b07cb9d5aca87ec501a52c743bea
Mohammad Tasin <tasinkamal78@gmail.com>
2025-11-10 15:43:48 +0600

    Update graph_dfs_bfs.cpp

--- graph_dfs.cpp
+++ graph_dfs.cpp
@@ -1,65 +1,84 @@
-#include <bits/stdc++.h>
+
+#### https://ideone.com/PCk6Az ###
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int visit[100];
-vector<int>v[100];
+int visitt[100];
+vector<int> v[100];
 
-void init(int n)
-{
-
-    for(int i=1;i<=n;i++)
-    {
-        visit[i] = 0;
-        v[i].clear();
+void init( int n){
+    for(int i=1;i<=n;i++){
+        visitt[i]=0;
     }
 }
 
-void dfs(int node)
-{
-    cout << node << "\n";
-    visit[node] = 1;
+void dfs(int node){
 
-    for(int i=0;i<v[node].size();i++)
-    {
-        if(visit[v[node][i] ]== 0)
-        {
-            dfs(v[node][i]);
+    visitt[node]=1;
+    for(int i=0;i<v[node].size();i++){
+        if(visitt[ v[node][i] ]==0){
+                dfs(v[node][i]);
         }
+    }
+    cout<<node<<"\n";
+}
+
+void bfs(int node){
+
+    queue<int> q;
+    q.push(node);
+    visitt[node]=1;
+
+    while(!q.empty()){
+       int now= q.front();
+       q.pop();
+       cout<<now<<"\n";
+       for(int i=0;i<v[now].size();i++){
+         if(visitt[v[now][i]]==0){
+                q.push(v[now][i]);
+                visitt[v[now][i]]=1;
+         }
+       }
     }
 }
 
-int main() {
 
+int main(){
 
-    freopen("list.txt","r",stdin);
-    freopen("out.txt","w",stdout);
+freopen("list.txt","r",stdin);
+freopen("out.txt","w",stdout);
 
-    int vertex,edge;  cin >> vertex >> edge;
-    init(vertex);
+int vert,e;
+cin>>vert>>e;
+init(vert);
 
-    vector<int>v[vertex+1];
-
-    for(int i=0;i<edge;i++)
-    {
-        int a,b; cin >> a >> b;
-
-        if(a==b){ v[a].push_back(b); }
-        else
-        {
-            v[a].push_back(b);
-            v[b].push_back(a);
-        }
+for(int i=0;i<e;i++){
+    int a,b;
+    cin>>a>>b;
+    if(a==b){v[a].push_back(b);}
+    else{
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
+}
 
-    dfs(1);
-    for(int i=1;i<=vertex;i++)
-    {
-        cout << i << " -> ";
-        for(int j = 0;j<v[i].size();j++)
-        {
-            cout << v[i][j] << " ";
-        }
-        cout << endl;
+
+//vector output
+for(int i=1;i<=vert;i++){
+    cout<<i<<" : ";
+    for(int j=0;j<v[i].size();j++){
+        printf("%d ", v[i][j]);
     }
-    return 0;
+    cout<<"\n";
+}
+cout<<"The output of DFS:\n";
+dfs(1);
+
+cout<<"The output of BFS:\n";
+init(vert);
+bfs(1);
+
+return 0;
 }
